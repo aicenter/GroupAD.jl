4108189be5ffcd18712b5cf30e3bc6cb74ab6906
Michaela Mašková <47153855+masenka31@users.noreply.github.com>
2021-03-23 09:28:40 +0100

    Update vae_instance.sh

--- scripts/experiments_point_cloud/vae_instance.sh
+++ scripts/experiments_point_cloud/vae_instance.sh
@@ -12,4 +12,4 @@ CONTAMINATION=$5
 module load Julia/1.5.1-linux-x86_64
 module load Python/3.8.2-GCCcore-9.3.0
 
-julia ./vas_instance.jl ${MAX_SEED} $DATASET ${ANOMALY_CLASSES} $METHOD $CONTAMINATION
+julia ./vae_instance.jl ${MAX_SEED} $DATASET ${ANOMALY_CLASSES} $METHOD $CONTAMINATION
