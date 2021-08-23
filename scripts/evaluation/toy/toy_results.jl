9f443c1bd5413b0f5a52ebcca12079835f7a1daf
masenka31 <masenka.maskova@gmail.com>
2021-08-23 10:30:45 +0200

    toy finished, images

--- scripts/evaluation/toy/toy_results.jl
+++ scripts/evaluation/toy/toy_results.jl
@@ -15,8 +15,8 @@ include(scriptsdir("evaluation", "workflow.jl"))
 include(srcdir("evaluation", "utils.jl"))
 include(scriptsdir("evaluation", "toy", "workflow.jl"))
 
-modelnames = ["knn_basic", "vae_basic", "MGMM", "vae_instance", "statistician", "PoolModel"]
-modelscores = [:distance, :score, :score, :type, :type, :type]
+modelnames = ["knn_basic", "vae_basic", "vae_instance", "statistician", "PoolModel", "MGMM"]
+modelscores = [:distance, :score, :type, :type, :type, :score]
 
 """
     groupedbar_matrix(df::DataFrame; group::Symbol, cols::Symbol, value::Symbol, groupnamefull=true)
@@ -103,18 +103,22 @@ for (modelname, group) in map((x, y) -> (x, y), modelnames, modelscores)
 end
 
 ### ALL MODELS TOGETHER
+# this is not formated the best
 df_all = DataFrame[]
-for (modelname, group) in pairs(toy_results_names_scores)
+for (modelname, group) in map((m, s) -> (m, s), modelnames, modelscores)
     df = sort(toy_results_collection[modelname], :val_AUC_mean, rev=true)
     df[!, :modelname] = repeat([modelname], nrow(df))
     best = vcat(map(x -> DataFrame(x[1,:]), groupby(df, :scenario))...)
     push!(df_all, best)
 end
 df_all = vcat(df_all..., cols=:union)
+sort!(df_all, :scenario)
 
+# better formating
 groupnames, M, labels = groupedbar_matrix(df_all, group=:scenario, cols=:modelname, value=:test_AUC_mean, groupnamefull=false)
+modellabels = ["kNNagg" "VAEagg" "VAE" "NS" "PoolModel" "MGMM"]
 groupedbar(
-    groupnames, M, labels=labels, size=(800,400), color_palette=:tab20,
+    groupnames, M, labels=modellabels, size=(800,400), color_palette=:tab20,
     ylabel="test AUC", xlabel="scenario", legendtitle="Model", legend=:outerright,
     left_margin=5Plots.mm, bottom_margin=5Plots.mm
 )
@@ -127,13 +131,115 @@ df = vcat(map(x -> find_best_model_scores("vae_instance", dataset, x; groupsymbo
 modelname = "PoolModel"
 df = vcat(map(x -> find_best_model_scores("PoolModel", dataset, x; groupsymbol=:type), 1:3)...)
 modelname = "statistician"
+scorelabels = [""]
 df = vcat(map(x -> find_best_model_scores("statistician", dataset, x; groupsymbol=:type), 1:3)...)
 toy_results_collection[modelname] = df
 
 groupnames, M, labels = groupedbar_matrix(df, group=:scenario, cols=:type, value=:test_AUC_mean, groupnamefull=false)
+
 groupedbar(
     groupnames, M, labels=labels, size=(800,400), color_palette=:tab20,
     ylabel="test AUC", xlabel="scenario", legendtitle="Score", legend=:outerright,
     left_margin=5Plots.mm, bottom_margin=5Plots.mm
 )
 savefig(plotsdir("toy", "groupedbar_$modelname.png"))
+
+######################################
+### Formating of individual models ###
+######################################
+
+# for NS and VAE
+modelname = "vae_instance"
+modelname = "statistician"
+df = toy_results_collection[modelname]
+groupnames, M, labels = groupedbar_matrix(df, group=:scenario, cols=:type, value=:test_AUC_mean, groupnamefull=false)
+idx = [8, 5, 4, 1, 7, 6, 3, 2, 11, 9, 10]
+new_labels = labels[idx]
+new_labels = ["sum" "mean" "max" "logU" "Po" "Po + logU" "LN" "LN + logU" "Chamfer" "MMD-G" "MMD-IMQ"]
+MM = M[:, idx]
+
+groupedbar(
+    groupnames, MM, labels=new_labels, size=(800,400), color_palette=:tab20,
+    ylabel="test AUC", xlabel="scenario", legendtitle="Score", legend=:outerright,
+    left_margin=5Plots.mm, bottom_margin=5Plots.mm
+)
+savefig(plotsdir("toy", "groupedbar_$modelname.png"))
+
+# for PoolModel
+modelname = "PoolModel"
+df = toy_results_collection[modelname]
+groupnames, M, labels = groupedbar_matrix(df, group=:scenario, cols=:type, value=:test_AUC_mean, groupnamefull=false)
+idx = [3,1,2]
+new_labels = labels[idx]
+new_labels = ["Chamfer" "MMD-G" "MMD-IMQ"]
+MM = M[:, idx]
+
+groupedbar(
+    groupnames, MM, labels=new_labels, color_palette=:tab20,
+    ylabel="test AUC", xlabel="scenario", legendtitle="Score", legend=:outerright,
+    left_margin=5Plots.mm, bottom_margin=5Plots.mm
+)
+savefig(plotsdir("toy", "groupedbar_$modelname.png"))
+
+# for knn model
+modelname = "knn_basic"
+df = toy_results_collection[modelname]
+groupnames, M, labels = groupedbar_matrix(df, group=:scenario, cols=:distance, value=:test_AUC_mean, groupnamefull=false)
+
+groupedbar(
+    groupnames, MM, labels=labels, color_palette=:tab20,
+    ylabel="test AUC", xlabel="scenario", legendtitle="Score", legend=:outerright,
+    left_margin=5Plots.mm, bottom_margin=5Plots.mm
+)
+savefig(plotsdir("toy", "groupedbar_$modelname.png"))
+
+# for vae_basic
+modelname = "vae_basic"
+df = toy_results_collection[modelname]
+groupnames, M, labels = groupedbar_matrix(df, group=:scenario, cols=:score, value=:test_AUC_mean, groupnamefull=false)
+idx = [3,1,2]
+new_labels = labels[idx]
+new_labels = ["rec" "rec-sampled" "rec-mean"]
+MM = M[:, idx]
+
+groupedbar(
+    groupnames, MM, labels=new_labels, color_palette=:tab20,
+    ylabel="test AUC", xlabel="scenario", legendtitle="Score", legend=:outerright,
+    left_margin=5Plots.mm, bottom_margin=5Plots.mm
+)
+savefig(plotsdir("toy", "groupedbar_$modelname.png"))
+
+# for MGMM
+modelname = "MGMM"
+df = toy_results_collection[modelname]
+groupnames, M, labels = groupedbar_matrix(df, group=:scenario, cols=:score, value=:test_AUC_mean, groupnamefull=false)
+idx = [1,3,2]
+new_labels = labels[idx]
+new_labels = ["point" "topic" "point + topic"]
+MM = M[:, idx]
+
+groupedbar(
+    groupnames, MM, labels=new_labels, color_palette=:tab20,
+    ylabel="test AUC", xlabel="scenario", legendtitle="Score", legend=:outerright,
+    left_margin=5Plots.mm, bottom_margin=5Plots.mm
+)
+savefig(plotsdir("toy", "groupedbar_$modelname.png"))
+
+# PoolModel on its own
+
+
+# for PoolModel
+modelname = "PoolModel"
+df = vcat(map(x -> find_best_model_scores("PoolModel", dataset, x; groupsymbol=:poolf), 1:3)...)
+groupnames, M, labels = groupedbar_matrix(df, group=:scenario, cols=:poolf, value=:test_AUC_mean, groupnamefull=false)
+idx = [6,5,1,2,4,3]
+new_labels = labels[idx]
+new_labels = ["mean" "max" "meanmax" "meanmax + card" "sumstat" "sumstat + card"]
+MM = M[:, idx]
+
+groupedbar(
+    groupnames, MM, labels=new_labels, color_palette=:tab20,
+    ylabel="test AUC", xlabel="scenario", legendtitle="Score", legend=:outerright,
+    left_margin=5Plots.mm, bottom_margin=5Plots.mm
+)
+savefig(plotsdir("toy", "groupedbar_$(modelname)_poolf.png"))
--- scripts/evaluation/toy/workflow.jl
+++ scripts/evaluation/toy/workflow.jl
@@ -73,3 +73,46 @@ function combined_dataframe(modelname, dataset)
     metricsnames = [:val_AUC, :val_AUPRC, :test_AUC, :test_AUPRC]
     cdf = combine(g, map(x -> x => mean, metricsnames))
 end
+
+function find_best_model(folder::String; metric=:val_AUC)
+    #folder = datadir("experiments", "contamination-0.0", modelname, dataset)
+    data = results_dataframe(folder)
+    point = load(collect_scores(folder)[1])
+    params = point[:parameters]
+
+    g = groupby(data, [keys(params)...])
+    un = unique(map(x -> size(x), g))
+    if length(un) != 1
+        idx = findall(x -> size(x,1) > 5, g)
+        @warn "There are groups with different sizes (different number of seeds). Possible duplicate models or missing seeds.
+        Removing $(length(g) - length(idx)) groups out of $(length(g)) with less than 6 seeds."
+        g = g[idx]
+    end
+
+    metricsnames = [:val_AUC, :val_AUPRC, :test_AUC, :test_AUPRC]
+    cdf = combine(g, map(x -> x => mean, metricsnames))
+    sort!(cdf, :val_AUC_mean, rev=true)
+    best_model = cdf[1,:]
+end
+function find_best_model(folder, groupkey, metric=:val_AUC)
+    #folder = datadir("experiments", "contamination-0.0", modelname, dataset, "scenario=$scenario")
+    data = GroupAD.Evaluation.results_dataframe(folder)
+    point = load(GroupAD.Evaluation.collect_scores(folder)[1])
+    params = point[:parameters]
+
+    g_score = groupby(data, groupkey)
+    g = map(x -> groupby(x, [keys(params)...]), g_score)
+    un = unique(vcat(map(x -> unique(map(y -> size(y), x)), g)...))
+
+    if length(un) != 1
+        idx = findall.(x -> size(x,1) > 5, g)
+        @warn "There are groups with different sizes (different number of seeds). Possible duplicate models or missing seeds.
+        Removing $(sum(length.(g)) - sum(length.(idx))) groups out of $(sum(length.(g))) with less than 6 seeds."
+        g = map(i -> g[i][idx[i]], 1:length(g))
+    end
+
+    metricsnames = [:val_AUC, :val_AUPRC, :test_AUC, :test_AUPRC]
+    cdf = map(y -> combine(y, map(x -> x => mean, metricsnames)), g)
+    cdf_sorted = map(x -> sort(x, :val_AUC_mean, rev=true), cdf)
+    best_models = vcat(map(x -> DataFrame(x[1,:]), cdf_sorted)...)
+end
