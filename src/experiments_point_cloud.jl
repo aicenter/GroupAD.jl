cc7a6de75f0a2cd41b528ad1af0072e3c2b52716
Matěj Zorek <34610021+TheMrGhostman@users.noreply.github.com>
2022-06-27 20:33:09 +0200

    setvae
    new:
    1) added point_cloud_experimental_loop_gpu
    2) compability with GenerativeMIL
    
    todo:
    1) gpu experimental for MIL problems

--- src/experiments_point_cloud.jl
+++ src/experiments_point_cloud.jl
@@ -98,6 +98,82 @@ function point_cloud_experimental_loop(sample_params_f, fit_f, edit_params_f,
 	(try_counter == max_tries) ? (@info "Reached $(max_tries) tries, giving up.") : nothing
 end
 
+# TODO test this
+function point_cloud_experimental_loop_gpu(sample_params_f, fit_f, edit_params_f, 
+	max_seed::Vector{Int}, modelname, dataset, contamination, savepath, anomaly_classes::Vector{Int}, method)
+	# this function is modified version of point_cloud_experimental_loop
+	# max_seed and anomaly_classes is expected to be list of Ints.
+
+	try_counter = 0
+	max_tries = 10*length(max_seed)
+	while try_counter < max_tries
+		# sample the random hyperparameters
+		parameters = sample_params_f()
+		for seed in max_seed
+			for class in anomaly_classes
+				data = load_data(dataset, anomaly_class_ind=class, seed=seed, method=method, contamination=contamination)
+				if method == "leave-one-in"
+					data = GroupAD.leave_one_in(data; seed=seed)
+				elseif method == "leave-one-out"
+					data = GroupAD.leave_one_out(data; seed=seed)
+				else
+					error("This model can only run on point cloud datasets!")
+				end
+
+				# define where data is going to be saved
+				_savepath = joinpath(savepath, "$(modelname)/$(dataset)/$(method)/class_index=$(class)/seed=$(seed)")
+				mkpath(_savepath)
+
+				#edit params
+				edited_parameters = edit_params_f(data, parameters, class, method)
+				
+				@info "Trying to fit $modelname on $(dataset) in $method setting.\nModel parameters: $(edited_parameters)..."
+				@info "Train/validation/test splits: $(size(data[1][1], 2)) | $(size(data[2][1], 2)) | $(size(data[3][1], 2))"
+				@info "Number of features: $(size(data[1][1], 1))"
+
+				# check if a combination of parameters and seed alread exists
+				if check_params(_savepath, edited_parameters)
+					@info "Params check done. Trying to fit."
+					# fit
+					training_info, results = fit_f(data, edited_parameters)
+					
+					# save the model separately			
+					if training_info.model !== nothing
+						modelf = joinpath(_savepath, savename("model", edited_parameters, "bson", digits=5))
+						tagsave(
+							modelf, 
+							Dict("model"=>training_info.model,
+								"fit_t"=>training_info.fit_t,
+								"history"=>training_info.history,
+								"parameters"=>edited_parameters
+								), 
+							safe = true)
+						(@info "Model saved to $modelf")
+
+						training_info = merge(training_info, (model = nothing, history=nothing))
+					end
+
+					# here define what additional info should be saved together with parameters, scores, labels and predict times
+					save_entries = merge(training_info, (modelname = modelname, seed = seed, dataset = dataset))
+
+					# now loop over all anomaly score funs
+					# TODO test this / SetVae should go to experiment()
+					@time for result in results
+						if modelname in ["vae_instance", "statistician", "PoolModel"]
+							experiment_bag(result..., data, _savepath; save_entries...)
+						else
+							experiment(result..., data, _savepath; save_entries...) # should work for setvae
+					end
+					try_counter = max_tries + 1
+				else
+					@info "Model already present, trying new hyperparameters..."
+					try_counter += 1
+				end
+			end
+		end
+	end
+	(try_counter == max_tries) ? (@info "Reached $(max_tries) tries, giving up.") : nothing
+end
 
 """
 	basic_experimental_loop_toy(sample_params_f, fit_f, edit_params_f, 
